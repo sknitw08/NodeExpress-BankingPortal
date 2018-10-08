f8191a51ea1ea04c9fdb022a09a8e4cafa656f2c
Sanjay Kumar <sank@microsoft.com>
2018-10-08 23:30:49 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -8,8 +8,33 @@ app.set('view engine', 'ejs'); // Set which view engine to set(pug, ejs)
 app.set('views', path.join(__dirname, 'views')); // Set location of view(ejs files)
 app.use(express.static(path.join(__dirname, 'public'))); // Set location for static files
 
+const accountData = fs.readFileSync(path.join(__dirname,'json/accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname,'json/users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', (req,res)=>{
-    res.render('index', { title : 'Index'});
+    res.render('index', { 
+        title : 'Account Summary',
+        accounts
+    });
+})
+
+app.get('/savings', (req,res) => {
+    res.render('account', { account : accounts.savings });
+})
+
+app.get('/checking', (req,res) => {
+    res.render('account', { account : accounts.checking });
+})
+
+app.get('/credit', (req,res) => {
+    res.render('account', { account : accounts.credit });
+})
+
+app.get('/profile', (req,res) => {
+    res.render('profile', { user : users[0] });
 })
 
 app.listen(3000, () => {
