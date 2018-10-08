c536be9fd6f6f17f411890a0dfc912424ad64b73
Sanjay Kumar <sank@microsoft.com>
2018-10-09 00:34:26 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('view engine', 'ejs'); // Set which view engine to set(pug, ejs)
 app.set('views', path.join(__dirname, 'views')); // Set location of view(ejs files)
 app.use(express.static(path.join(__dirname, 'public'))); // Set location for static files
+app.use(express.urlencoded({extended:true}));
 
 const accountData = fs.readFileSync(path.join(__dirname,'json/accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
@@ -19,23 +20,48 @@ app.get('/', (req,res)=>{
         title : 'Account Summary',
         accounts
     });
-})
+});
 
 app.get('/savings', (req,res) => {
     res.render('account', { account : accounts.savings });
-})
+});
 
 app.get('/checking', (req,res) => {
     res.render('account', { account : accounts.checking });
-})
+});
 
 app.get('/credit', (req,res) => {
     res.render('account', { account : accounts.credit });
-})
+});
 
 app.get('/profile', (req,res) => {
     res.render('profile', { user : users[0] });
-})
+});
+
+app.get('/transfer', (req,res) => {
+    res.render('transfer');
+});
+
+app.post('/transfer', (req,res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: "Transfer Completed"});
+});
+
+app.get('/payment', (req,res) => {
+    res.render('payment', {account: accounts.credit});
+});
+
+app.post('/payment', (req,res) => {
+    accounts['credit'].balance -= req.body.amount;
+    accounts['credit'].available = parseInt(accounts['credit'].available) + parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+});
+
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
